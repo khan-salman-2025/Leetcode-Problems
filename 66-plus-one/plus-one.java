a620448238aad9d64fe8bc9f97bc2482edf6ff95
khan-salman-2025 <mohd.salman4071@gmail.com>
2026-09-15 15:07:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (61.18%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int idx = digits.length;
+        for(int i = idx - 1; i >= 0; i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            }else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] arr = new int[idx + 1];
+        arr[0] = 1;
+        return arr;
+    }
+}
